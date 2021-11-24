5924557c286f3be3bc03560ebe69287a6a0c9aac
JunwuHuang <978162622@qq.com>
2021-11-24 14:21:08 +0800

    feat(ui): remove Tag component

--- packages/ui/src/Tag/types.ts
+++
@@ -1,9 +0,0 @@
-import type { CSSProperties, ReactNode } from 'react'
-
-export interface TagProps {
-  className?: string
-  style?: CSSProperties
-  checked?: boolean
-  onChange?: (checked: boolean) => void
-  children?: ReactNode
-}
